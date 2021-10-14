da1c55af792303fa76d9ae411ab275eef453f04f
AlejandroMiranda <alejandromsantiago@outlook.com>
2021-10-14 02:18:31 +0200

    files managements changes

--- configConstants.py
+++ configConstants.py
@@ -1,5 +1,16 @@
+FILES_INI_FILE_NAME="files_path.ini"
 
-class EngineSimulationDataPath:
-    ROOT :str= 'DATAPATH'
-    TRAIN_DATA_PATH :str = "trainDataPath"
-    TEST_DATA_PATH : str = "testDataPath"
+
+class EngineSimulationConstants:
+    """
+        Constants for files
+    """
+    ROOT :str= 'DATAPATHS'
+    TRAIN_DATA_FOLDER :str = "trainDataPath"
+    TEST_DATA_FOLDER: str = "testDataPath"
+    #Folder name which contains the processed train data
+    TRAIN_DATA_OUTPUT_FOLDER : str ="outputTrainDataFolder"
+    OUTPUT_FILE_NAME : str ="outputFileName"
+
+    PATTERNS_ROOT="PATTERNS"
+    TRAIN_FILES_NAME_PATTERN="trainFilePattern"
